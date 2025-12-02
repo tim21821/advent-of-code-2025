c3ee4425c1152f88bf873fdbe053a21bcfe74a02
tim21821 <tim.mattick@gmx.de>
2025-12-02 17:46:03 +0100

    Use regex for day 2, part 2

--- src/day2.jl
+++ src/day2.jl
@@ -1,22 +1,11 @@
+const REPEAT_RE = r"^(.+)\1+$"
+
 function isinvalid(i::Int)
     str = string(i)
     return length(str) % 2 == 0 && str[1:(length(str)÷2)] == str[(length(str)÷2+1):end]
 end
 
-function isinvalid(str::AbstractString, n::Int)
-    len = length(str)
-    if len % n != 0
-        return false
-    end
-
-    sequence = str[1:(len÷n)]
-    for i in 2:n
-        if str[((i-1)*(len÷n)+1):(i*(len÷n))] != sequence
-            return false
-        end
-    end
-    return true
-end
+isinvalid2(i::Int) = occursin(REPEAT_RE, string(i))
 
 function part1()
     input = open("input/day2.txt") do f
@@ -46,12 +35,8 @@ function part2()
     for range in ranges
         firstID, secondID = parse.(Int, split(range, '-'))
         for id in firstID:secondID
-            str = string(id)
-            for i in 2:length(str)
-                if isinvalid(str, i)
-                    invalidsum += id
-                    break
-                end
+            if isinvalid2(id)
+                invalidsum += id
             end
         end
     end
