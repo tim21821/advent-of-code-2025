3bb94a8d6f3287e552b5a9f69b577170614b18c5
tim21821 <tim.mattick@gmx.de>
2025-12-08 22:32:03 +0100

    Use heap instead of queue

--- src/day8.jl
+++ src/day8.jl
@@ -12,17 +12,17 @@ function part1()
 
     boxes = [SVector{3,Int}(parse.(Int, split(line, ','))) for line in lines]
 
-    queue = PriorityQueue{Tuple{Int,Int},Float64}()
+    heap = BinaryHeap{Tuple{Int,Int,Float64}}(Base.By(last))
     for i in eachindex(boxes)
         for j in (i+1):length(boxes)
-            push!(queue, (i, j) => norm(boxes[i] - boxes[j]))
+            push!(heap, (i, j, norm(boxes[i] - boxes[j])))
         end
     end
 
     graph = SimpleGraph(length(boxes))
     for _ in 1:NUM_CONNECTIONS
-        connection, _ = popfirst!(queue)
-        add_edge!(graph, connection...)
+        connection = pop!(heap)
+        add_edge!(graph, connection[1], connection[2])
     end
     components = connected_components(graph)
     sort!(components, by = length, rev = true)
@@ -36,17 +36,17 @@ function part2()
 
     boxes = [SVector{3,Int}(parse.(Int, split(line, ','))) for line in lines]
 
-    queue = PriorityQueue{Tuple{Int,Int},Float64}()
+    heap = BinaryHeap{Tuple{Int,Int,Float64}}(Base.By(last))
     for i in eachindex(boxes)
         for j in (i+1):length(boxes)
-            push!(queue, (i, j) => norm(boxes[i] - boxes[j]))
+            push!(heap, (i, j, norm(boxes[i] - boxes[j])))
         end
     end
 
     dsu = IntDisjointSet(length(boxes))
     while true
-        connection, _ = popfirst!(queue)
-        union!(dsu, connection...)
+        connection = pop!(heap)
+        union!(dsu, connection[1], connection[2])
         if num_groups(dsu) == 1
             return boxes[connection[1]][1] * boxes[connection[2]][1]
         end
